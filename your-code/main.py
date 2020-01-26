e58780f0f091f970005944db50c3c8bdcde8f292
Cleanspin <sin@lucky>
2020-01-26 23:37:49 +0000

    a meaningful message

--- your-code/main.py
+++ your-code/main.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # coding: utf-8
 
 # # Introduction to Pandas Lab
-#
+# 
 # Complete the following set of exercises to solidify your knowledge of Pandas fundamentals.
 
 # ### 1. Import Numpy and Pandas and alias them to `np` and `pd` respectively.
 
-# In[2]:
+# In[1]:
 
 
 import numpy as np
@@ -16,32 +16,32 @@ import pandas as pd
 
 # ### 2. Create a Pandas Series containing the elements of the list below.
 
-# In[3]:
+# In[2]:
 
 
 lst = [5.7, 75.2, 74.4, 84.0, 66.5, 66.3, 55.8, 75.7, 29.1, 43.7]
 
 
-# In[ ]:
+# In[3]:
 
 
-
+df = pd.Series(lst)
+df
 
 
 # ### 3. Use indexing to return the third value in the Series above.
-#
+# 
 # *Hint: Remember that indexing begins at 0.*
 
-# In[9]:
+# In[4]:
 
 
-df = pd.DataFrame(lst)
 df.loc[2]
 
 
 # ### 4. Create a Pandas DataFrame from the list of lists below. Each sublist should be represented as a row.
 
-# In[11]:
+# In[5]:
 
 
 b = [[53.1, 95.0, 67.5, 35.0, 78.4],
@@ -56,7 +56,7 @@ b = [[53.1, 95.0, 67.5, 35.0, 78.4],
      [73.7, 39.0, 43.2, 81.6, 34.7]]
 
 
-# In[13]:
+# In[6]:
 
 
 df = pd.DataFrame(b)
@@ -65,13 +65,13 @@ df
 
 # ### 5. Rename the data frame columns based on the names in the list below.
 
-# In[14]:
+# In[7]:
 
 
 colnames = ['Score_1', 'Score_2', 'Score_3', 'Score_4', 'Score_5']
 
 
-# In[15]:
+# In[8]:
 
 
 df = pd.DataFrame(b, columns = colnames)
@@ -80,41 +80,40 @@ df
 
 # ### 6. Create a subset of this data frame that contains only the Score 1, 3, and 5 columns.
 
-# In[20]:
+# In[9]:
 
 
-from itertools import compress
-df = pd.DataFrame(list(compress(b, [1,0,1,0,1])), columns = list(compress(colnames, [1,0,1,0,1])))
-df
+sdf = df.iloc[:,::2]
+sdf
 
 
 # ### 7. From the original data frame, calculate the average Score_3 value.
 
-# In[ ]:
+# In[10]:
 
 
-
+df['Score_3'].mean()
 
 
 # ### 8. From the original data frame, calculate the maximum Score_4 value.
 
-# In[ ]:
+# In[13]:
 
 
-
+df['Score_4'].max()
 
 
 # ### 9. From the original data frame, calculate the median Score 2 value.
 
-# In[ ]:
+# In[14]:
 
 
-
+df['Score_2'].median()
 
 
 # ### 10. Create a Pandas DataFrame from the dictionary of product orders below.
 
-# In[ ]:
+# In[11]:
 
 
 orders = {'Description': ['LUNCH BAG APPLE DESIGN',
@@ -132,24 +131,28 @@ orders = {'Description': ['LUNCH BAG APPLE DESIGN',
  'Revenue': [1.65, 13.2, 1.65, 518.4, 2.5, 5.95, 11.95, 19.8, 1.9, 60.0]}
 
 
-# In[ ]:
+# In[16]:
 
 
-
+O = pd.DataFrame(orders)
+O
 
 
 # ### 11. Calculate the total quantity ordered and revenue generated from these orders.
 
-# In[ ]:
+# In[22]:
 
 
-
+print(
+O['Quantity'].sum(),
+O['Revenue'].sum()
+)
 
 
 # ### 12. Obtain the prices of the most expensive and least expensive items ordered and print the difference.
 
-# In[ ]:
+# In[23]:
 
 
-
+print(O['UnitPrice'].max()-O['UnitPrice'].min())
 
